ec467614b6d029585bd30fea48edfbe704917dab
Ethan Charpentier <tanou@mac.home>
2025-04-16 23:13:36 +0200

    feat (raycast - camera): adding a Z axis to the camera to look at every direction up down right left

--- src/raycast/drawer.c
+++ src/raycast/drawer.c
@@ -7,25 +7,27 @@
 
 #include "frame.h"
 
-void draw_floor_and_ceiling(sfRenderWindow *window)
+void draw_floor_and_ceiling(frame_t *frame)
 {
     sfColor floor_color = sfColor_fromRGB(100, 100, 100);
     sfColor ceiling_color = sfColor_fromRGB(50, 50, 50);
     static sfRectangleShape* floor_rect = NULL;
     static sfRectangleShape* ceiling_rect = NULL;
+    int vertical = (int)(WINDOWY * tanf(PLAYER->vertical_angle) / 2);
 
     if (!floor_rect)
         floor_rect = sfRectangleShape_create();
     sfRectangleShape_setFillColor(floor_rect, floor_color);
-    sfRectangleShape_setPosition(floor_rect, (sfVector2f){0, WINDOWY / 2});
-    sfRectangleShape_setSize(floor_rect, (sfVector2f){WINDOWX, WINDOWY / 2});
-    sfRenderWindow_drawRectangleShape(window, floor_rect, NULL);
+    sfRectangleShape_setPosition(floor_rect, v2f(0, WINDOWY / 2 + vertical));
+    sfRectangleShape_setSize(floor_rect, v2f(WINDOWX, WINDOWY / 2 - vertical));
+    sfRenderWindow_drawRectangleShape(WINDOW, floor_rect, NULL);
     if (!ceiling_rect)
         ceiling_rect = sfRectangleShape_create();
     sfRectangleShape_setFillColor(ceiling_rect, ceiling_color);
-    sfRectangleShape_setPosition(ceiling_rect, (sfVector2f){0, 0});
-    sfRectangleShape_setSize(ceiling_rect, (sfVector2f){WINDOWX, WINDOWY / 2});
-    sfRenderWindow_drawRectangleShape(window, ceiling_rect, NULL);
+    sfRectangleShape_setPosition(ceiling_rect, v2f(0, 0));
+    sfRectangleShape_setSize(ceiling_rect,
+        v2f(WINDOWX, WINDOWY / 2 + vertical));
+    sfRenderWindow_drawRectangleShape(WINDOW, ceiling_rect, NULL);
 }
 
 void render_wall_column(sfRenderWindow *window, int column,
@@ -42,7 +44,7 @@ void render_wall_column(sfRenderWindow *window, int column,
 }
 
 void render_wall_column_textured(frame_t *frame, sfVector2f column_wall_height,
-    sfVector2f hits, bool hit_vertical)
+    sfVector2f hits, sfVector2i vertical)
 {
     int tex_x = 0;
     float scale_y = 0;
@@ -51,7 +53,7 @@ void render_wall_column_textured(frame_t *frame, sfVector2f column_wall_height,
     sfIntRect tex_rect = {0, 0, 1, (int)tex_size.y};
 
     sfSprite_setTexture(wall_sprite, frame->game->map->walltexture, sfTrue);
-    if (hit_vertical)
+    if ((bool) vertical.x)
         tex_x = (int)fmodf(hits.y, TILE_SIZE);
     else
         tex_x = (int)fmodf(hits.x, TILE_SIZE);
@@ -59,7 +61,7 @@ void render_wall_column_textured(frame_t *frame, sfVector2f column_wall_height,
     tex_rect = irct(tex_x, 0, 1, (int)tex_size.y);
     sfSprite_setTextureRect(wall_sprite, tex_rect);
     sfSprite_setPosition(wall_sprite, v2f((float)column_wall_height.x,
-        (WINDOWY - column_wall_height.y) / 2));
+        (WINDOWY - column_wall_height.y) / 2 + vertical.y));
     scale_y = column_wall_height.y / (float)tex_size.y;
     sfSprite_setScale(wall_sprite, v2f(1.0f, scale_y));
     sfRenderWindow_drawSprite(WINDOW, wall_sprite, NULL);
