59dd585291d6128adeefb8f32245aa21c8e5fbed
EthanCharpentier667 <ethan.charpentier@epitech.eu>
2025-05-21 13:53:02 +0200

    fix (fixed_objects): verify if the object is in the window to draw it

--- src/raycast/fixed_object.c
+++ src/raycast/fixed_object.c
@@ -121,6 +121,8 @@ static void render_segment(frame_t *frame, render_context_t *ctx,
 
     if (segment->corrected_dist < 0.1f)
         return;
+    if (segment->screen_y < 0 || segment->screen_y > WINDOWY)
+        return;
     if (frame->z_buffer[segment->screen_x] < segment->corrected_dist)
         return;
     sprite = create_segment_sprite(segment, ctx->object,
