dce8688f03b81318887e26d222dd55be5b9f6ac2
Noe Caillaud <noe.caillaud@epitech.eu>
2025-05-22 17:36:37 +0200

    feat (weapon): knife refactored to comply new code

---
+++ src/weapon/knife_behavior.c
@@ -0,0 +1,80 @@
+/*
+** EPITECH PROJECT, 2025
+** Wolf3D-action
+** File description:
+** Knife weapon behavior
+*/
+
+#include "frame.h"
+
+static bool attempt_knife_damage(weapon_t *weapon, frame_t *frame)
+{
+    enemy_t *target = find_enemy_in_range(frame);
+
+    if (target) {
+        damage_enemy(frame, target, weapon->damage);
+        return true;
+    }
+    return false;
+}
+
+static void handle_knife_idle(weapon_t *weapon)
+{
+    weapon->current_frame = 0;
+    if ((sfMouse_isButtonPressed(sfMouseLeft) ||
+        sfKeyboard_isKeyPressed(sfKeyL)) &&
+        weapon->attack_cooldown <= 0) {
+        weapon->state = WEAPON_ATTACKING;
+        weapon->animation_timer = 0;
+        weapon->current_frame = 0;
+    }
+}
+
+static void handle_knife_attacking(weapon_t *weapon,
+    frame_t *frame, float delta_time)
+{
+    weapon->animation_timer += delta_time;
+    if (weapon->animation_timer >= 0.05f) {
+        weapon->animation_timer = 0;
+        weapon->current_frame++;
+        if (weapon->current_frame == 2)
+            attempt_knife_damage(weapon, frame);
+        if (weapon->current_frame >= weapon->total_frames) {
+            weapon->state = WEAPON_COOLDOWN;
+            weapon->attack_cooldown = 0.3f;
+            weapon->current_frame = 0;
+            weapon->animation_timer = 0;
+        }
+    }
+}
+
+static void handle_knife_cooldown(weapon_t *weapon)
+{
+    if (weapon->attack_cooldown <= 0) {
+        weapon->state = WEAPON_IDLE;
+        weapon->current_frame = 0;
+    }
+}
+
+void update_knife_behavior(weapon_t *weapon,
+    frame_t *frame, float delta_time)
+{
+    if (weapon->attack_cooldown > 0) {
+        weapon->attack_cooldown -= delta_time;
+    }
+    switch (weapon->state) {
+        case WEAPON_IDLE:
+            handle_knife_idle(weapon);
+            break;
+        case WEAPON_ATTACKING:
+            handle_knife_attacking(weapon, frame, delta_time);
+            break;
+        case WEAPON_COOLDOWN:
+            handle_knife_cooldown(weapon);
+            break;
+        default:
+            weapon->state = WEAPON_IDLE;
+            weapon->current_frame = 0;
+            break;
+    }
+}
--- src/weapon/update_weapon.c
+++ src/weapon/update_weapon.c
@@ -26,91 +26,50 @@ void damage_enemy(frame_t *frame, enemy_t *enemy, int damage)
     }
 }
 
-static void update_weapon_cooldown(weapon_t *weapon, float delta_time)
+static void update_weapon_visual(weapon_t *weapon)
 {
-    if (weapon->attack_cooldown > 0)
-        weapon->attack_cooldown -= delta_time;
-}
+    sfIntRect frame_rect = {0, 0, 0, 0};
 
-static void handle_idle_state(weapon_t *weapon)
-{
-    weapon->current_frame = 0;
-    if ((sfMouse_isButtonPressed(sfMouseLeft) ||
-        sfKeyboard_isKeyPressed(sfKeyL)) &&
-        weapon->attack_cooldown <= 0) {
-        weapon->state = WEAPON_ATTACKING;
-        weapon->animation_timer = 0;
-        weapon->current_frame = 0;
+    if (weapon->state == WEAPON_FIRING && weapon->alt_texture) {
+        sfSprite_setTexture(weapon->sprite, weapon->alt_texture, sfFalse);
+        frame_rect = irct(weapon->current_frame * weapon->frame_width,
+            0, weapon->frame_width, weapon->frame_height);
+        sfSprite_setTextureRect(weapon->sprite, frame_rect);
+    } else {
+        sfSprite_setTexture(weapon->sprite, weapon->texture, sfFalse);
+        frame_rect = irct(weapon->current_frame * weapon->frame_width,
+            0, weapon->frame_width, weapon->frame_height);
+        sfSprite_setTextureRect(weapon->sprite, frame_rect);
     }
+    sfSprite_setPosition(weapon->sprite, weapon->position);
 }
 
-static void attempt_weapon_damage(frame_t *frame, weapon_t *weapon)
+void handle_weapon_input(frame_t *frame)
 {
-    enemy_t *target = find_enemy_in_range(frame);
-
-    if (target)
-        damage_enemy(frame, target, weapon->damage);
-}
-
-static void handle_attack_state(weapon_t *weapon,
-    frame_t *frame, float delta_time)
-{
-    weapon->animation_timer += delta_time;
-    if (weapon->animation_timer >= 0.05f) {
-        weapon->animation_timer = 0;
-        weapon->current_frame++;
-        if (weapon->current_frame == 2)
-            attempt_weapon_damage(frame, weapon);
-        if (weapon->current_frame >= weapon->total_frames) {
-            weapon->state = WEAPON_COOLDOWN;
-            weapon->attack_cooldown = 0.3f;
-            weapon->current_frame = 0;
-            weapon->animation_timer = 0;
-        }
+    if (sfKeyboard_isKeyPressed(sfKeyC)) {
+        switch_weapon(frame, 0);
+    } else if (sfKeyboard_isKeyPressed(sfKeyV)) {
+        switch_weapon(frame, 1);
     }
-}
-
-static void handle_cooldown_state(weapon_t *weapon)
-{
-    if (weapon->attack_cooldown <= 0) {
-        weapon->state = WEAPON_IDLE;
-        weapon->current_frame = 0;
+    if (sfKeyboard_isKeyPressed(sfKeyR)) {
+        reload_weapon(frame);
     }
-}
-
-static void update_weapon_visual(frame_t *frame, weapon_t *weapon)
-{
-    sfIntRect frame_rect = {
-        weapon->current_frame * weapon->frame_width,
-        0,
-        weapon->frame_width,
-        weapon->frame_height
-    };
-    sfVector2u window_size = sfRenderWindow_getSize(WINDOW);
-    sfVector2f weapon_pos = {
-        window_size.x / 1.4f - (weapon->frame_width * weapon->scale.x) / 2.0f,
-        window_size.y - (weapon->frame_height * weapon->scale.y)
-    };
-
-    sfSprite_setTextureRect(weapon->sprite, frame_rect);
-    sfSprite_setPosition(weapon->sprite, weapon_pos);
 }
 
 void update_weapon(frame_t *frame, float delta_time)
 {
-    weapon_t *weapon = HUD->weapon;
+    weapon_t *weapon = CURRENT_WEAPON;
+    bool trigger_pressed = sfMouse_isButtonPressed(sfMouseLeft) ||
+        sfKeyboard_isKeyPressed(sfKeyL);
 
-    update_weapon_cooldown(weapon, delta_time);
-    switch (weapon->state) {
-        case WEAPON_IDLE:
-            handle_idle_state(weapon);
-            break;
-        case WEAPON_ATTACKING:
-            handle_attack_state(weapon, frame, delta_time);
-            break;
-        case WEAPON_COOLDOWN:
-            handle_cooldown_state(weapon);
-            break;
-    }
-    update_weapon_visual(frame, weapon);
+    if (!weapon)
+        return;
+    handle_weapon_input(frame);
+    if (trigger_pressed && !weapon->is_trigger_held)
+        weapon->is_trigger_held = true;
+    else if (!trigger_pressed && weapon->is_trigger_held)
+        weapon->is_trigger_held = false;
+    if (weapon->update_behavior)
+        weapon->update_behavior(weapon, frame, delta_time);
+    update_weapon_visual(weapon);
 }
