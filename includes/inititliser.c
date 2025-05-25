bf73a26db271c85f39be57e3107ef2657b4ff2c8
EthanCharpentier667 <ethan.charpentier@epitech.eu>
2025-05-25 13:37:49 +0200

    feat (heal - item): added a new image to the item 'heal' now size is 0.8x0.8, rest unchangd in initialiser.c

--- includes/inititliser.c
+++ includes/inititliser.c
@@ -25,7 +25,7 @@ const struct item_infos_s ITEM_INFOS[] = {
         {-1, -1, -1, -1}, "Key", true, false, "The Key Of The Door"},
     {RES "key.png", {0.5, 0.5}, {250, 280, -0.50},
         {-1, -1, -1, -1}, "Key", true, false, "The Key Of The Door"},
-    {RES "heal.png", {0.4, 0.4}, {450, 300, -0.50},
+    {RES "heal.png", {0.8, 0.8}, {450, 300, -0.50},
         {-1, -1, -1, -1}, "Heal", true, true, "Heal of 20 HP"},
     {RES "ammo_box.png", { 0.5, 0.5}, {450, 350, -0.50},
         {-1, -1, -1, -1}, "Ammo_box", true, true,
