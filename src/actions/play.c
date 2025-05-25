0fb025bebacdd12dfb2ee1f565a2a18336b0d71a
EthanCharpentier667 <ethan.charpentier@epitech.eu>
2025-05-25 18:11:56 +0200

    feat (save - load): adding a name for your saves

--- src/actions/play.c
+++ src/actions/play.c
@@ -14,14 +14,66 @@ int do_resume(frame_t *frame)
     return 0;
 }
 
+static bool get_name(frame_t *frame, FILE *fp, char *buffer)
+{
+    char *sswolfs_part = NULL;
+
+    buffer[strcspn(buffer, "\n")] = 0;
+    sswolfs_part = strstr(buffer, "sswolfs/");
+    if (sswolfs_part != NULL) {
+        frame->save = strdup(sswolfs_part);
+    } else {
+        system("zenity --error --text=\"La sauvegarde"
+            " doit être dans le dossier sswolfs/\"");
+        pclose(fp);
+        return false;
+    }
+    frame->name = strdup(sswolfs_part + strlen("sswolfs/"));
+    return true;
+}
+
+static bool get_save_name(frame_t *frame)
+{
+    FILE *fp = popen("zenity --file-selection --save --filename=\"hilter\" "
+        "--title=\"Nom de la partie\"", "r");
+    char buffer[256] = {0};
+
+    if (fp == NULL) {
+        fprintf(stderr, "Failed to run zenity command\n");
+        return -1;
+    }
+    if (fgets(buffer, sizeof(buffer) - 1, fp) != NULL) {
+        if (!get_name(frame, fp, buffer)) {
+            pclose(fp);
+            return get_save_name(frame);
+        }
+        return true;
+    }
+    pclose(fp);
+    return false;
+}
+
+static void check_save(frame_t *frame)
+{
+    if (!frame->played) {
+        create_save_directory();
+        if (get_save_name(frame)) {
+            change_scene(frame, GAME);
+            sfRenderWindow_setMouseCursorVisible(WINDOW, sfFalse);
+            sfMouse_setPositionRenderWindow(v2i(WINDOWX / 2,
+                WINDOWY / 2), WINDOW);
+            frame->played = true;
+        }
+    } else {
+        frame->game->level = 1;
+        change_scene(frame, GAME);
+    }
+}
+
 int do_play(frame_t *frame)
 {
     UI->pause = false;
     PLAYER->pause = false;
-    change_scene(frame, GAME);
-    frame->game->level = 1;
-    frame->played = true;
-    sfRenderWindow_setMouseCursorVisible(WINDOW, sfFalse);
-    sfMouse_setPositionRenderWindow(v2i(WINDOWX / 2, WINDOWY / 2), WINDOW);
+    check_save(frame);
     return 0;
 }
